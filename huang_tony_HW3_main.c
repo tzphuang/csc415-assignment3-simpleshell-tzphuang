e57270295bf6201053dd40cbf4dee82e8ea8d6b7
Student <thuang54@mail.ccsf.edu>
2021-09-17 18:19:14 -0700

    EOF/ERROR for fgets working, assignment3 still in progress

--- huang_tony_HW3_main.c
+++ huang_tony_HW3_main.c
@@ -18,9 +18,16 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define BUFFER_SIZE										180
+
+
 
 int main(int argc, char *argv[]) 
 {
+	// to run this put into the command line
+	// ./huang_tony_HW3_main & 
+	// the & puts the process in the background
+	/*
 	int result = fork();
 	switch (result)
 	{
@@ -48,6 +55,61 @@ int main(int argc, char *argv[])
 			sleep(3);
 		}
 		break;
+	}
+	*/
+
+	//buffer will hold the user's command inputs
+	char buffer[BUFFER_SIZE+1];
+	//printf("Buffer size of buffer is %d \n", BUFFER_SIZE+1);
+
+	//parsedBuffer size should be able to hold a line of 180 bytes
+	//if those bytes are separated by spaces, then you get 90 valid bytes
+	//and we need 1 extra byte for null terminator
+	char * parsedBuffer[(BUFFER_SIZE/2) +1];
+	//printf("Buffer size of buffer is %d \n", (BUFFER_SIZE/2)+1);
+
+	char * strExit = "exit";
+
+	char * returnFgets;
+
+	//while loop continues to take in input from user until user exits
+	while(1){
+		printf("Please input your command line!\n");
+		returnFgets = fgets(buffer, BUFFER_SIZE, stdin);
+
+		//if fgets() returns null we either have end of file or an error
+		//we then check which one it is and proceed accordingly
+		if (NULL == returnFgets){
+			//this can be forcefully activated when you input ctrl + d into the command line
+			if(feof(stdin)){
+				printf("End of file! Exiting Gracefullly.\n");
+				break;
+			}
+
+			if(ferror(stdin)){
+				printf("Error! Input error for fgets().");
+				break;
+			}
+		}
+
+		printf("Buffer now contains: %s, and stringComparison is: %s\n", buffer, strExit);
+
+		int stringComparison = strcasecmp(buffer, strExit);
+		printf ("string Comparison is: %d\n", stringComparison);
+
+		stringComparison = strncasecmp(buffer, strExit, 4);
+		printf ("string n Comparison is: %d\n", stringComparison);
+
+		//string comparison for the first 4 characters to see if its "EXIT"
+		if(	strncasecmp(strExit, buffer, 4)	== 0){
+			printf("User has input \"Exit\", program will exit gracefully.\n");
+			break;
+		}
+		else{
+			printf("User input was: \"%s\"\n", buffer);
+		}
+
+		
 	}
 
 	
